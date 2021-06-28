5cd82a2f9140b25f34476c380387d40b89aed114
Gideon Kiplangat <gideonkiplangat4@gmail.com>
2021-06-28 17:21:51 +0300

    concate variables

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ person_info = {
     'age': '22 Years'
     }
 print('Person information: ', person_info)
+# concation of variables
+first_name = 'Gideon'
+last_name = 'Kiplangat'
+country = 'Kenya'
+city = 'Nairobi'
+age = '22 years'
+is_married = False
+skills = ['HTML', 'CSS', 'JS', 'java', 'Python']
+print('First Name: '+first_name+'\n'+'Last name: '+last_name+'\n'+'Country of residence: '+country+'\n'+'City:'+city+'\n'+'Age: '+age)
