a64d51a2b0170d0f1cfe23b5bd0a9389dc8d301b
giddy22-02 <56033401+giddy22-02@users.noreply.github.com>
2021-09-23 19:23:15 +0300

    Update if_else.py

--- if_else.py
+++ if_else.py
@@ -1,7 +1,7 @@
-# comparing the two variables
-a= 5
-b= 45
-if a<b:
-    print('a is less than b')
-elif a>b:
-    print('a is greter than b')
+#Line1
+phrase ="Riara"
+print(phrase)
+print(len(phrase))
+
+# Line2
+print('Riara')
