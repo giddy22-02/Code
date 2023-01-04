424a344c9e57962da106ae11e33c46cc3fa507fe
Gideon Kiplangat <56033401+gkiplangat@users.noreply.github.com>
2023-01-04 11:56:41 +0300

    Added a dictionary     count the frequency of names

--- list.py
+++ list.py
@@ -8,6 +8,19 @@ print(lst[0])
 lst[0] = 28
 print(lst)
 
-# Empty list
-courses = list()
-print(courses)
+ # count the frequency of names
+ # list of names are Gideon, Andrew, Benard, Selah, Mercy, Erick, Faith
+
+counts = dict()
+
+names =['Gideon', 'Andrew', 
+'Benard', 'Selah', 'Mercy', 
+'Gideon', 'Andrew', 'Erick',
+'Gideon', 'Faith',]
+
+for name in names:
+    if name not in counts:
+        counts[name] = 1
+    else:
+        counts[name] = counts[name]+1
+print(counts)
