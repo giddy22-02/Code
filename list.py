0884395638777a16a92d610661bb47635a467fe9
Gideon Kiplangat <56033401+gkiplangat@users.noreply.github.com>
2023-01-04 11:58:41 +0300

    added

--- list.py
+++ list.py
@@ -7,20 +7,3 @@ lst.append(25)
 print(lst[0])
 lst[0] = 28
 print(lst)
-
- # count the frequency of names
- # list of names are Gideon, Andrew, Benard, Selah, Mercy, Erick, Faith
-
-counts = dict()
-
-names =['Gideon', 'Andrew', 
-'Benard', 'Selah', 'Mercy', 
-'Gideon', 'Andrew', 'Erick',
-'Gideon', 'Faith',]
-
-for name in names:
-    if name not in counts:
-        counts[name] = 1
-    else:
-        counts[name] = counts[name]+1
-print(counts)
