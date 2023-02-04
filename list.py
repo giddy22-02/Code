a5db64cd357064f89edd4913d8cd5184a43c782c
gkiplangat <gkiplangat>
2023-02-04 18:56:19 +0300

    creted a new file called variable2.py

--- list.py
+++
@@ -1,9 +0,0 @@
-lst = list()
-lst.append(22)
-lst.append(23)
-lst.append(24)
-lst.append(25)
-
-print(lst[0])
-lst[0] = 28
-print(lst)
---
+++ variable2.py
@@ -0,0 +1,37 @@
+name = "YouTube"
+print("The word is: "+name)
+print("Character in index 0 is: "+name[0])
+print("Character in index 3 is: "+name[3])
+print("Character in index -1 is: "+name[-1])
+print("Character in index -2 is: "+name[-2])
+
+#printing characters in range
+#print characters in range 1:4 in word "YouTube"
+#printing begins with "o" and end with T in index 3
+#It will print "ouT"
+#Why?
+#Because indexing begins from 0
+#Y = index 0,
+#o = index 1
+
+print("Characters in index 1:4 is: "+name[1:4])
+
+#[1:]-(means from index 1 to the end)
+#it will print "outTube"
+
+print("Characters in index 1: is: "+name[1:])
+
+#[:4]-(means from index 0 to index 3)
+
+print("Characters in index :4 is: "+name[:4])
+
+#[3:10]-(means from index 3 to the end despite not have 10 characters)
+print("Characters in index 3:10 is: "+name[3:10])
+
+#create a variable (name)
+name = "Gideon Kiplangat"
+print("Your name is: "+name)
+
+#print the lenght of characters in variable name
+print(len(name))
+
