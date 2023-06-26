88766b2fbcec93ccc9d69e475e005c94089d6279
gkiplangat <gkiplangat>
2023-06-27 00:43:42 +0300

    feat:created math functions

---
+++ broCode/math_func.py
@@ -0,0 +1,14 @@
+import math
+
+pi =3.14 
+x =5
+y =8
+z =4
+print(pi)
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(min(x,y,z))
+print(max(x,y,z))
+print(math.sqrt(pi))
+print(pow(2,3))
